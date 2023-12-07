03b06f05e2337abc0eff00218c3f21954129d07a
Vladislav <boss.zimovet@gmail.com>
2023-12-07 17:18:53 +0300

    add function createBoxes

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const btn = document.getElementById('btn')
 
 btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
 
+createBoxes = () => {
+    for (let i = 0; i < 4; i++){
+        for(let j = 0; j < 4; j++){
+            const box = document.createElement('div')
+            box.classList.add('box')
+            box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+            boxesContainer.appendChild(box)
+        }
+    }
+}
